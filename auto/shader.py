a58347ec37bd450da85da5b63c91b3c7b5de8706
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2021-12-26 09:56:03 -0600

    add shader template

--- auto/shader.py
+++ auto/shader.py
@@ -10,6 +10,21 @@ executeConditionalPath(
         "uniform float iTime;\n" +
         "uniform vec3 iResolution;\n" +
         "varying vec3 fragCoord;\n\n" +
+        "void main()\n" +
+        "{\n" +
+        "    // Normalized pixel coordinates (from 0 to 1)\n" +
+        "    // - .5 so center of the screen is the center of the screen\n" +
+        "    vec2 uv = fragCoord.xy / iResolution.xy - .5;\n" +
+        "\n" +
+        "    // Sets ratio into 1:1\n" +
+        "    uv.x *= iResolution.x / iResolution.y;\n" +
+        "\n" +
+        "    // Time varying pixel color\n" +
+        "    vec3 col = 0.5 + 0.5*cos(iTime + uv.xyx + vec3(0,2,4));\n" +
+        "\n" +
+        "    // Output to screen\n" +
+        "    if (distance(uv, vec2(0.)) < .5) gl_FragColor = vec4(col,1.0);\n" +
+        "}\n" +
         "`;\n"
     )
 )
